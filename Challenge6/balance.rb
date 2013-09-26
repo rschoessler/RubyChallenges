99fbbdfd0ebe60c8700982cd67a4f884ac8ea5da
rschoessler <rschoessler@gmail.com>
2013-09-26 15:36:51 -0400

    Adding Challenge6 files

--- Challenge6/balance.rb
+++ Challenge6/balance.rb
@@ -8,12 +8,77 @@
 #from 14 and up to 22, you subtract 9
 
 class Balance
+  ROCK1 = 1
+  ROCK3 = 3
+  ROCK9 = 9
+  ROCK27 = 27
 
     def initialize
-      @rock1 = 1
-      @rock3 = 3
-      @rock9 = 9
-      @rock27 = 27
+           #do nothing
+    end
+
+    def addToNonGrain(weightToAdd,non_grain_side)
+
+    end
+
+    def addToGrainSide(weightToAdd,grain_side)
+      grain_side = grain_side +  weightToAdd
+      return grain_side
+    end
+
+    def calculate(grain_side, non_grain_side)
+      #this is the algorithm to determine which weights go on which sides of the balance
+      #it should return the grain side and the non-grain side as a hash
+
+      arrayGrain = Array.new
+      arrayNonGrain = Array.new
+
+
+=begin
+      begin
+        if grain_side == 1
+         #add the 1 to the non-grain side
+          non_grain_side += ROCK1
+          difference = non_grain_side - grain_side
+        elsif grain_side > 13
+          #add 27 to the non-grain side
+          non_grain_side += ROCK27
+          difference = non_grain_side - grain_side
+        elsif grain_side == 13
+          #add 9 to the grain side
+          grain_side += ROCK9
+          difference = non_grain_side - grain_side
+        elsif grain_side < 13
+          #
+
+
+        end
+=end
+
+      begin
+        if grain_side == 1
+          #add 1 to the non-grain side
+          non_grain_side += ROCK1
+          difference = non_grain_side - grain_side
+        end
+
+
+
+
+
+
+        puts "Non-grain side:  #{non_grain_side}"
+        puts "Grain side:  #{grain_side}"
+        puts "Difference:  #{difference}"
+
+        arrayGrain << grain_side
+        arrayNonGrain << non_grain_side
+      end until difference == 0
+
+
+
+      return {:grain => arrayGrain, :non_grain => arrayNonGrain}
+
     end
 
 
--- Challenge6/weight.rb
+++ Challenge6/weight.rb
@@ -1,7 +1,12 @@
+require_relative "balance"
+
 #this is the main file for this challenge
 
 
 #ok, let's prompt the user for an amount of grain
+
+$non_grain_side = 0    #sets the initial value of the weight on the grain side of the balance
+$grain_side = 0         #sets the initial value of the grain side of the balance
 
 puts "How much grain do you have? (between 1 and 40 pounds)"
 grainWeight = gets.chomp.to_i
@@ -10,5 +15,15 @@ if grainWeight > 40 or grainWeight < 1
   puts "I can only measure weights between 1 and 40 pounds!"
 end
 
-puts grainWeight
+#puts grainWeight
+
+balance = Balance.new
+$grain_side = balance.addToGrainSide(grainWeight,$grain_side)
+
+puts "You now have #{$grain_side} pounds of grain on the scale."
+
+arrayBalance = balance.calculate($grain_side, $non_grain_side)
+
+puts arrayBalance[:grain]     #an array in a hash
+puts arrayBalance[:non_grain] #an array in a hash
 
